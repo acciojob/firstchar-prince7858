aa612d6842edf9270aa274d71b6f917648df259c
Prince Kumar <91559607+prince7858@users.noreply.github.com>
2024-11-30 23:16:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function firstChar(text) {
   // your code here
-	 if (text.length < 0) {
+	 if (text == "" || text ==" ") {
 		 return "";
   } else {
     return text.charAt(0); 
@@ -9,5 +9,5 @@ function firstChar(text) {
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-const text = prompt("Enter text:");
+//const text = prompt("Enter text:");
 alert(firstChar(text));
